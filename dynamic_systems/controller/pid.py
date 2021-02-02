6573a598227834adfcb1179235fc00d32c936416
Eduardo <edumontesuma@gmail.com>
2021-02-01 22:22:19 -0300

    Adding recursive implementation for PID controller

--- dynamic_systems/controller/pid.py
+++ dynamic_systems/controller/pid.py
@@ -2,14 +2,14 @@ class PID:
     def __init__(self, Kp, Ki, Kd, dt, ref,
                  min_action=0.0,
                  max_action=1.0,
-                 pos_ref=True):
+                 pos_ref=True,
+                 initial_action=0.0,
+                 implementation="direct"):
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
         self.dt = dt
-        self.memory_err = 0.0
         self.history_err = []
-        self.history_obs = []
         self.Pterm = 0.0
         self.Iterm = 0.0
         self.Dterm = 0.0
@@ -18,26 +18,43 @@ class PID:
         self.ref = ref
         self.last_t = 0.0
         self.pos_ref = pos_ref
+        self.last_action = initial_action
+        self.last_err = 0.0
+        self.implementation = implementation
 
     def __call__(self, t, observation):
         if self.pos_ref:
             err = self.ref - observation
         else:
             err = observation - self.ref
+        self.history_err.append(err)
         dt = t - self.last_t
 
-        self.Pterm = self.Kp * err
-        self.Iterm += self.Ki * err * self.dt
-        if dt > 0.0:
-            self.Dterm = self.Kd * (err - self.memory_err) / self.dt
+        if self.implementation == "direct":
+            self.Pterm = self.Kp * self.history_err[-1]
+            self.Iterm += self.Ki * self.history_err[-1] * self.dt
+            if dt > 0.0 and len(self.history_err) > 3:
+                self.Dterm = self.Kd * (self.history_err[-1] - self.history_err[-2]) / self.dt
+            else:
+                self.Dterm = 0.0
+            
+            action = self.Pterm + self.Iterm + self.Dterm
+            action = self.saturate(action)
+        elif self.implementation == "recursive":
+            self.Iterm = self.Ki * dt * self.history_err[-1]
+            if len(self.history_err) > 2:
+                self.Pterm = self.Kp * (self.history_err[-1] - self.history_err[-2])
+            else:
+                self.Pterm = 0.0
+            if len(self.history_err) > 3 and dt > 0.0:
+                self.Dterm = self.Kd * (self.history_err[-1] - 2 * self.history_err[-2] + self.history_err[-3]) / dt
+            else:
+                self.Dterm = 0.0
+            action = self.last_action + self.Pterm + self.Iterm + self.Dterm
+            action = self.saturate(action)
         else:
-            self.Dterm = 0.0
-
-        self.history_err.append(err)
-        self.history_obs.append(observation)
-        self.memory_err = err
-        action = self.Pterm + self.Iterm + self.Dterm
-        action = self.saturate(action)
+            raise ValueError("Invalid implementation. Should be either 'direct' or 'recursive', but got {}".format(self.implementation))
+        self.last_action = action
         self.last_t = t
         return action
 
--- dynamic_systems/simulation/cstr.py
+++ dynamic_systems/simulation/cstr.py
@@ -18,9 +18,10 @@ class CSTR(AbstractSystem):
                  Cp=1.0,
                  Cpc=1.0,
                  N=1.0,
-                 process_noise=0.00,
+                 process_noise=1e-2,
                  measurement_noise=0.00,
-                 linearize=False):
+                 fault_type=0):
+        # Disturbances
         self.Ci = 1.0
         self.Ti = 350.0
         self.Tci = 350.0
@@ -38,14 +39,20 @@ class CSTR(AbstractSystem):
         self.Cp = Cp
         self.Cpc = Cpc
         self.N = N
-        self.process_noise = process_noise
-        self.measurement_noise = measurement_noise
+        self.fault_type = fault_type
+        super().__init__(process_noise, measurement_noise)
 
     def dynamics(self, t, x, u):
         QV = self.Q / self.V
         k = self.k0 * np.exp(- self.ER / x[1])
-        a = self.a0
-        b = self.b0
+        if self.fault_type == 1:
+            a = self.a0 * np.exp(- 0.004 * t)
+        else:
+            a = self.a0
+        if self.fault_type == 2:
+            b = self.b0 * np.exp(- 0.005 * t)
+        else:
+            b = self.b0
         alpha = a * self.dHr * k / (self.rho * self.Cp)
         beta = b * self.UA / (self.rho * self.Cp * self.V)
         betac = b * self.UA / (self.rhoc * self.Cpc * self.Vc)
@@ -60,7 +67,7 @@ class CSTR(AbstractSystem):
         return dxdt
 
     def observe(self, t, x):
-        return x[1] + self.measurement_noise * np.random.randn()
+        return x[1]
 
     def saturate(self, x):
         x[0] = np.maximum(0, x[0])
@@ -74,9 +81,17 @@ class CSTR(AbstractSystem):
         observations = []
         actions = []
         for ti in t:
+            if self.fault_type == 10:
+                self.Ci = np.maximum(0, 0.005 * np.random.randn())
+            elif self.fault_type == 11:
+                self.Ti = 5 * np.random.randn()
+            elif self.fault_type == 12:
+                self.Tci = 5 * np.random.randn()
             state = np.array(X[-1])
-            observation = self.observe(ti, state)
-            action = u_fn(ti, observation)
+            action = u_fn(ti, self.observe(ti, state))
+            # Note: observation vector for the CSTR system is quite different from
+            # the two-tanks system.
+            observation = [self.Ci, self.Ti, self.Tci, *state, action]
 
             states.append(state)
             observations.append(observation)
@@ -97,12 +112,37 @@ class CSTR(AbstractSystem):
             k4 = self.dynamics(ti + dt, state_4, action)
             new_state = state + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
             new_state = self.saturate(new_state)
-            """
-            mid_state = X[-1] + self.dynamics(ti, state, action) * (dt / 2)
-            mid_state = self.saturate(mid_state)
-            new_state = mid_state + self.dynamics(ti, mid_state, action) * dt
-            new_state = self.saturate(mid_state)
-            """
 
             X.append(new_state)
-        return np.array(actions), np.array(states), np.array(observations)
+        actions, states, observations = np.array(actions), np.array(states), np.array(observations)
+        # Note: Values set according to the simulink plant of Pilario and Cao
+        # Adds measurement noise to Ci
+        observations[:, 0] += np.sqrt(0.000001) * np.random.randn(*observations[:, 0].shape)
+        # Adds measurement noise to Ti
+        observations[:, 1] += 0.05 * np.random.randn(*observations[:, 1].shape)
+        # Adds measurement noise to Tci
+        observations[:, 2] += 0.05 * np.random.randn(*observations[:, 2].shape)
+        # Adds measurement noise to C
+        observations[:, 3] += np.sqrt(0.000001) * np.random.randn(*observations[:, 3].shape)
+        # Adds measurement noise to T
+        observations[:, 4] += 0.05 * np.random.randn(*observations[:, 4].shape)
+        # Adds measurement noise to Tc
+        observations[:, 5] += 0.05 * np.random.randn(*observations[:, 5].shape)
+        # Adds measurement noise to Qc
+        observations[:, 6] += 0.2 * np.random.randn(*observations[:, 5].shape)
+
+        if self.fault_type == 3:
+            observations[:, 0] += 0.005 * t
+        elif self.fault_type == 4:
+            observations[:, 1] += 0.1 * t
+        elif self.fault_type == 5:
+            observations[:, 2] += 0.1 * t
+        elif self.fault_type == 6:
+            observations[:, 3] += 0.005 * t
+        elif self.fault_type == 7:
+            observations[:, 4] += 0.1 * t
+        elif self.fault_type == 8:
+            observations[:, 5] += 0.1 * t
+        elif self.fault_type == 9:
+            observations[:, 6] -= 0.2 * t
+        return actions, states, observations
